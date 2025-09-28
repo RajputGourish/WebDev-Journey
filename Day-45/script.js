7fd4ffaaeff2f6b2db9874a7d742141d8b092258
RajputGourish <gourishrajput000@gmail.com>
2025-09-28 20:43:26 +0530

    Javascript start

---
+++ Day-45/script.js
@@ -0,0 +1,24 @@
+// alert("hello world");
+
+console.log("Code is running...")
+console.log("Code is better...")
+console.log("Code is looking like a wao...")
+
+// var a = prompt("enter your number")
+
+// var istrue = confirm("Are you want to leave from here & blast your device")
+
+// if(istrue){
+//     console.log("Your device is blasting............")
+// }
+// else{
+//     console.log("Your device is not blasting............")
+// }
+
+
+// console.log("Your number is " + a)
+
+
+// document.title = "hey title sir"
+
+// document.body.style.backgroundColor="red"
