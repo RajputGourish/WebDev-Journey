03ab8d01996c60d86be956ee1411de60b4801f2c
RajputGourish <gourishrajput000@gmail.com>
2025-09-30 22:01:52 +0530

    javascript variables

---
+++ Day-46/script.js
@@ -0,0 +1,74 @@
+console.log("Hello this is Tutoral55 of Sigma Web Devlopment and Enjoy");
+
+// var a = 7;
+// var b = 13;
+// var c = "harry";
+// var _a2 = 44;
+// var $z = 114;
+
+// var 12a = 22;
+
+
+// console.log(a + b + 10)
+
+// console.log(typeof a, typeof b, typeof c)
+
+
+// const a1 = 12;
+// console.log("const a1 = " + a1)
+
+// a1 = a1 + 1;
+// console.log("const a1 = " + a1)
+
+
+//  Let 
+
+// let a = 7;
+// let b = 13;
+// let c = "harry";
+// let _a2 = 44;
+// let $z = 114;
+
+// console.log(b);
+
+// {
+//     let b = 14;
+//     console.log(b);
+
+// }
+// console.log(b);
+
+
+
+let x = "Gourish";
+let y = 12;
+let q = 0.13;
+let z;
+const p = true;
+let r = null;
+
+
+console.log(x, y, q, z, p, r);
+console.log(typeof x, typeof y, typeof q, typeof z, typeof p, typeof r);
+
+
+let o = {
+    "name": "harry",
+    age: 22,
+    "job code": 12141,
+    is_handsome: true
+}
+
+console.log(o);
+
+o.salary = "12crore";
+console.log(o);
+
+o.salary = "23crore";
+console.log(o);
+
+o.jobrole = "AM MD";
+console.log(o);
+
+o["real name"] = "Gourish";
+console.log(o);
