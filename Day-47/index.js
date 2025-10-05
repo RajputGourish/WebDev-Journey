ee47b1851a6130076bad178c8aa498708581c6a0
RajputGourish <gourishrajput000@gmail.com>
2025-10-05 12:22:49 +0530

    js conditional

---
+++ Day-47/index.js
@@ -0,0 +1,33 @@
+console.log("Hello world");
+
+let age = 0;
+// let grace = 2;
+
+// console.log(age + grace);
+// console.log(age - grace);
+// console.log(age * grace);
+// console.log(age / grace);
+// console.log(age ** grace);
+// console.log(age % grace);
+
+
+
+if (age == 18) {
+    console.log("You can Drive");
+}
+
+else if (age == 0) {
+    console.log("Are you kidding?");
+
+}
+else {
+    console.log("You can not drive");
+}
+
+
+let a = 6;
+let b = 4;
+
+let c = (a>b)?(a-b):(b-a);
+
+console.log(c);
