3fb97626be2c2ed9cd35dfe71af13006ccde6a00
RajputGourish <gourishrajput000@gmail.com>
2025-10-07 18:49:54 +0530

    js loops

---
+++ Day-48/index.js
@@ -0,0 +1,39 @@
+console.log("This is loop lec");
+console.log("----------------");
+
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(i + 1);
+// }  
+
+let obj = {
+    name: "Gourish",
+    age: 15,
+    role: "enginner",
+    color: "yellow"
+}
+
+// for (const key in obj) {
+//     const element = obj[key];
+//     // console.log(key, element);
+//     console.log(key);
+// }
+
+// for (const c of "Gourish") {
+//     console.log(c);    
+// }
+
+
+// let x = 1;
+
+// while (x < 6) {
+//     console.log(x);
+//     x++;
+// }
+
+
+let i = 10;
+
+do{
+    console.log(i);    
+}while(i<5)
