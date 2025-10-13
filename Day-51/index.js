1e1edab710c038e5a53ae328693e64b6b711d702
RajputGourish <gourishrajput000@gmail.com>
2025-10-13 09:00:24 +0530

    js strings

---
+++ Day-51/index.js
@@ -0,0 +1,52 @@
+console.log("this is sting tut");
+
+
+let a = "Gourish";
+
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+// console.log(a[7]);
+
+console.log(a.length);
+
+
+let real_name = "Gourish";
+let friend = "Ritik";
+
+console.log("His name is " + real_name + " and friend name is " + friend);
+
+console.log(`His name is ${real_name} and friend name is ${friend}`);
+
+console.log();
+
+
+// let s = `Ro\"han`;
+let s = `Rohan`;
+
+console.log(s);
+
+
+let b = "gourish";
+console.log(b.slice(0, 5));
+console.log(b.slice(1));
+
+console.log();
+
+console.log(b.replace("g", "77"));
+
+console.log(a.concat(s, "Sam", "Aman"));
+
+
+let inc = "  gourish   ";
+
+console.log(inc.trim());
+
+console.log();
+console.log(a);
+
+console.log(b.indexOf("g"));
