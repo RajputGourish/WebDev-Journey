fd6019022fe2f3b370162fccb3ecbbf9c621cbf0
RajputGourish <gourishrajput000@gmail.com>
2025-10-19 20:57:34 +0530

    js arrays

---
+++ Day-54/index.js
@@ -0,0 +1,51 @@
+console.log("hello world");
+let arr = [1, 2, 3, 4, 5]
+// index   0, 1, 2, 3, 4
+
+// arr[0] = 533;
+
+// console.log(arr);
+// console.log(arr.length);
+// console.log(typeof arr);
+// console.log(arr.toString());
+// console.log(arr.join(" and "));
+
+console.log(arr);
+// arr.pop();
+arr.push(12);
+arr.push("back");
+arr.shift();
+arr.unshift("Front");
+console.log(arr);
+
+// delete arr[6];
+// console.log(arr);
+
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+// console.log(arr[5]);
+
+
+// let a1 = [1, 2];
+// let a2 = [3, 4];
+// let a3 = [5, 8];
+// a1.concat(a2, a3);
+
+
+// let a1 = [4, 2,212,232,229, 1, 7, 354, 7, 23];
+// console.log(a1);
+// console.log(a1.sort());
+
+
+
+let number = [1, 2, 3, 4, 5, 6];
+console.log(number);
+
+// number.splice(2, 2)
+// number.splice(2, 2, 11, 12, 14)
+
+// console.log(number.slice(1, 4));
+console.log(number.reverse());
---
+++ Day-54/mfr.js
@@ -0,0 +1,42 @@
+let arr = [1, 13, 4, 5, 29, 12];
+
+// let narr = [];
+
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     narr.push(element ** 2);
+// }
+
+// let newarr = arr.map((e, index, array) => {
+//     return e ** 2;
+// });
+
+// console.log(arr);
+// console.log(newarr);
+
+const greaterthenseven = ((e) => {
+    if (e > 7) return true;
+    return false;
+});
+
+// console.log(arr.filter(greaterthenseven));
+
+
+// console.log(arr.filter((e) => {
+//     if (e > 7) return true;
+//     return false;
+// }));
+
+// let array = [1,2,3,4,5,6];
+
+// const red = ((a, b)=>{
+//     return a*b;
+// });
+
+// console.log(array.reduce(red));
+
+
+
+let z = Array.from("12312raj21323");
+
+console.log(z);
