cfe577b4243c7a4e29a4fd960d09a8d4985a906f
RajputGourish <gourishrajput000@gmail.com>
2025-10-29 20:37:11 +0530

    js DOM parent sibling

---
+++ Day-58/script.js
@@ -0,0 +1,29 @@
+console.log("hello world");
+
+document.body.childNodes;
+document.body.childNodes[0];
+document.body.childNodes[1];
+document.body.childNodes[2];
+document.body.childNodes[3];
+document.body.childNodes[1].childNodes;
+
+// document.body.childNodes[1].firstChild;
+// document.body.childNodes[1].lastChild;
+
+// document.body.childNodes[1].firstElementChild;
+// document.body.childNodes[1].lastElementChild;
+
+// document.body.childNodes[1].lastElementChild.parentElement;
+
+// document.body.firstElementChild.children;
+
+
+
+// let cont = document.body.childNodes;
+// cont.firstChild;
+
+
+
+
+
+
