97be81ef8e67d725231074b69b74f1bd18b4ee33
RajputGourish <gourishrajput000@gmail.com>
2026-01-01 23:36:24 +0530

    Use context in react

---
+++ Day-106/src/Without-Context-api/Components/Component1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Component1 = (props) => {
+  return (
+    <div>
+      {/* Component 1 */}
+      Count is {props.count}
+    </div>
+  )
+}
+
+export default Component1
