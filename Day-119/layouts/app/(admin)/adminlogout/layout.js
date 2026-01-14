1e9487b47ad76e092357359a4f8fecb4e74705e7
RajputGourish <gourishrajput000@gmail.com>
2026-01-14 19:57:45 +0530

    layouts in next js

---
+++ Day-119/layouts/app/(admin)/adminlogout/layout.js
@@ -0,0 +1,25 @@
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+export const metadata = {
+    title: "Logout Admin",
+    description: "This is my facebook app clone thanks and connect with the world and the nation get the world with the group and called it with the quote and kindly describe for the connection world",
+};
+
+export default function logoutLayout({ children }) {
+    return (
+        <>
+            <div>logout Navbar</div>
+            {children}
+        </>
+    );
+}
---
+++ Day-119/layouts/app/layout.js
@@ -0,0 +1,32 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Facebook - Connect with the world",
+  description: "This is my facebook app clone thanks and connect with the world and the nation get the world with the group and called it with the quote and kindly describe for the connection world",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
