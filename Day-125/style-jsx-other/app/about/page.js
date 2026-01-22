becd7ed48af4eaef5a1071837dc03da428e20ad1
RajputGourish <gourishrajput000@gmail.com>
2026-01-22 22:00:14 +0530

    style in next js

---
+++ Day-125/style-jsx-other/app/about/page.js
@@ -0,0 +1,36 @@
+"use client"
+import React from 'react'
+
+const about = () => {
+    return (
+        <>
+            <div className='container'>
+                <h1>Hey this is about me</h1>
+                <p>I am a good boy!</p>
+
+
+                {/* <style jsx global>{`
+
+                .container{
+                    background-color: red;
+                    color:white;
+                }
+                `} */}
+                <style jsx global>{`
+
+                .container{
+                    background-color: red;
+                    color:white;
+                }
+                `}
+                </style>
+
+            </div>
+            <div className="container">
+                Hey i am good
+            </div>
+        </>
+    )
+}
+
+export default about
