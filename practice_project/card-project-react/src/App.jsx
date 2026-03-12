bda927d971a5e17c15ab7b2fb18d360a2a8eef23
RajputGourish <gourishrajput000@gmail.com>
2026-03-12 22:02:20 +0530

    Final Cards using props

--- practice_project/card-project-react/src/App.jsx
+++ practice_project/card-project-react/src/App.jsx
@@ -1,22 +1,118 @@
 import React from 'react'
-import User from './Components/User'
+import Card from './Components/Card'
 
 const App = () => {
-  const arr = [{ user: "rohan" }, 
-               { user: "sohan" },
-               { user: "mohan" },
-               { user: "gourish" },
-               { user: "bholu" }];
+  const jobs = [
+    {
+      brandLogo: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg",
+      company: "Google",
+      datePosted: "5 days ago",
+      post: "Frontend Developer",
+      tag1: "Full Time",
+      tag2: "Junior Level",
+      pay: "$45/hr",
+      location: "Bangalore, India"
+    },
+    {
+      brandLogo: "https://upload.wikimedia.org/wikipedia/commons/0/05/Facebook_Logo_%282019%29.png",
+      company: "Meta",
+      datePosted: "1 week ago",
+      post: "React Developer",
+      tag1: "Full Time",
+      tag2: "Mid Level",
+      pay: "$50/hr",
+      location: "Mumbai, India"
+    },
+    {
+      brandLogo: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg",
+      company: "Amazon",
+      datePosted: "3 days ago",
+      post: "Software Engineer",
+      tag1: "Full Time",
+      tag2: "Junior Level",
+      pay: "$48/hr",
+      location: "Hyderabad, India"
+    },
+    {
+      brandLogo: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
+      company: "Apple",
+      datePosted: "2 weeks ago",
+      post: "UI Engineer",
+      tag1: "Part Time",
+      tag2: "Senior Level",
+      pay: "$60/hr",
+      location: "Delhi, India"
+    },
+    {
+      brandLogo: "https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png",
+      company: "Netflix",
+      datePosted: "4 days ago",
+      post: "Full Stack Developer",
+      tag1: "Full Time",
+      tag2: "Senior Level",
+      pay: "$70/hr",
+      location: "Remote"
+    },
+    {
+      brandLogo: "https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg",
+      company: "Microsoft",
+      datePosted: "10 days ago",
+      post: "Backend Developer",
+      tag1: "Full Time",
+      tag2: "Mid Level",
+      pay: "$55/hr",
+      location: "Noida, India"
+    },
+    {
+      brandLogo: "https://upload.wikimedia.org/wikipedia/commons/7/7b/Adobe_Systems_logo_and_wordmark.svg",
+      company: "Adobe",
+      datePosted: "6 days ago",
+      post: "JavaScript Developer",
+      tag1: "Part Time",
+      tag2: "Junior Level",
+      pay: "$40/hr",
+      location: "Pune, India"
+    },
+    {
+      brandLogo: "https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png",
+      company: "Uber",
+      datePosted: "8 days ago",
+      post: "Frontend Engineer",
+      tag1: "Full Time",
+      tag2: "Mid Level",
+      pay: "$52/hr",
+      location: "Bangalore, India"
+    },
+    {
+      brandLogo: "https://upload.wikimedia.org/wikipedia/commons/6/69/Airbnb_Logo_Bélo.svg",
+      company: "Airbnb",
+      datePosted: "2 days ago",
+      post: "Web Developer",
+      tag1: "Full Time",
+      tag2: "Junior Level",
+      pay: "$46/hr",
+      location: "Remote"
+    },
+    {
+      brandLogo: "https://upload.wikimedia.org/wikipedia/commons/b/bd/Tesla_Motors.svg",
+      company: "Tesla",
+      datePosted: "3 weeks ago",
+      post: "Frontend Engineer",
+      tag1: "Full Time",
+      tag2: "Senior Level",
+      pay: "$65/hr",
+      location: "Austin, USA"
+    }
+  ];
 
-  arr.forEach((e) => {
-    console.log(e);
-  })
 
   return (
-    <div className="parent p-[30px] bg-black h-[100vh] flex gap-[30px]">
-      {arr.map((e) => {
-        return <User name={e.user} />
-      })}
+    <div className="parent flex-wrap p-7.5 min-h-screen justify-center flex gap-7.5">
+      {jobs.map((e, idx) => {
+        return <div>
+          <Card companyName={e.company} logo={e.brandLogo} datepost={e.datePosted} postname={e.post} tag1={e.tag1} tag2={e.tag2} pay={e.pay} location={e.location} />
+        </div>
+        })}
     </div>
   )
 }
--- practice_project/card-project-react/src/Components/Card.jsx
+++ practice_project/card-project-react/src/Components/Card.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import { Bookmark } from 'lucide-react'
 
-const Card = () => {
+const Card = (props) => {
     return (
         <div className="card h-[330px] w-[280px] rounded-2xl p-[30px] bg-white flex flex-col justify-between">
             <div>
                 <div className="top flex justify-between items-center mb-[30px]">
 
-                    <img className='h-[40px] w-[40px] border-[#dadada] border p-[1px] rounded-full' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV5wT5R7vumVlrrcVIzxKqoqNYtNmpJqwMIQ&s" alt="amazon img" />
+                    <img className='h-[40px] w-[40px] border-[#dadada] border p-[1px] rounded-full object-contain' src={props.logo} alt="amazon img" />
                     <button className='flex items-center border border-[#dadada] py-[3px] px-[6px] text-[10px] rounded-[5px] gap-[3px] bg-transparent text-[#b1b1b1]'>Save <Bookmark size={12} /></button>
 
                 </div>
                 <div className="center">
-                    <h3 className='text-[17px] font-medium'>Amazon <span className='text-[9px] font-medium text-[#8b8b8b]'>5 days ago</span></h3>
+                    <h3 className='text-[17px] font-medium'>{props.companyName} <span className='text-[9px] font-medium text-[#8b8b8b]'>{props.datepost}</span></h3>
 
-                    <h2 className='text-[20px] font-bold'>Senior UI/UX Designer</h2>
+                    <h2 className='text-[20px] font-bold'>{props.postname}</h2>
                     <div className='tag flex gap-[5px] mt-[10px]'>
-                        <h4 className='text-[12px] bg-[#dadada] text-[#111] font-medium px-[4px] py-[8px] rounded-[5px]'>Part Time</h4>
-                        <h4 className='text-[12px] bg-[#dadada] text-[#111] font-medium px-[4px] py-[8px] rounded-[5px]'>Senior Level</h4>
+                        <h4 className='text-[12px] bg-[#dadada] text-[#111] font-medium px-[4px] py-[8px] rounded-[5px]'>{props.tag1}</h4>
+                        <h4 className='text-[12px] bg-[#dadada] text-[#111] font-medium px-[4px] py-[8px] rounded-[5px]'>{props.tag2}</h4>
                     </div>
 
                 </div>
@@ -25,10 +25,10 @@ const Card = () => {
             </div>
             <div className="bottom flex items-center justify-between border-t-[1px] border-[#d7d7d7ce] pt-[15px]">
                 <div>
-                    <h3 className='text-[17px]'>$120/hr</h3>
-                    <p className='text-[10px] text-[#]'>Mumbai, India</p>
+                    <h3 className='text-[17px]'>{props.pay}</h3>
+                    <p className='text-[10px] text-[#]'>{props.location}</p>
                 </div>
-                <button className='bg-black text-white px-2 py-1 border-none rounded-[5px] font-bold'>Apply now</button>
+                <button className='cursor-pointer hover:opacity-70 bg-black text-white px-2 py-1 border-none rounded-[5px] font-bold'>Apply now</button>
             </div>
         </div>
     )
