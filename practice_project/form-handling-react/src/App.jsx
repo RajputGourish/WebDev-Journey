55892f71a55db1b868b77b5891c802a4e15858a8
RajputGourish <gourishrajput000@gmail.com>
2026-03-20 18:44:36 +0530

    Learn Two way binding

--- practice_project/form-handling-react/src/App.jsx
+++ practice_project/form-handling-react/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [name, setName] = useState('')
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log("Form Submited successfully by", name);
+
+    setName('');
+  }
+
   return (
     <div>
-      <form > 
-          <input type="text" placeholder='Enter your Name'/>
-          <button>Submit</button>
+      <form onSubmit={(e) => submitHandler(e)}>
+        <input
+          type="text"
+          placeholder='Enter your Name'
+          value={name}
+          onChange={(e) => { setName(e.target.value)}} />
+        <button>Submit</button>
       </form>
     </div>
   )
